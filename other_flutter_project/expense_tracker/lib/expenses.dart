0133daa0b606cd21b395b634ce74651309ddb657
zobaer53 <hossain.zobaer11858@gmail.com>
2023-11-30 17:38:52 +0600

    expenseTracker-- expense list demo value

--- other_flutter_project/expense_tracker/lib/expenses.dart
+++ other_flutter_project/expense_tracker/lib/expenses.dart
@@ -2,6 +2,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'models/expense.dart';
+
 class Expenses  extends StatefulWidget{
   const Expenses({super.key});
 
@@ -14,6 +16,11 @@ class Expenses  extends StatefulWidget{
 }
 
 class _ExpensesState extends State<Expenses>{
+  final List<Expense> _registerdExpenses = [
+    Expense(title: 'Flutter Course', amount: 19.99, date:DateTime.now(), category: Category.work),
+    Expense(title: 'Cinema', amount: 15.99, date:DateTime.now(), category: Category.leisure)
+  ];
+
   @override
   Widget build(BuildContext context) {
   return const Scaffold(
