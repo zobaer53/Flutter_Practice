04289ff211fa6c4c254ee7717fb95db196d35afb
zobaer53 <hossain.zobaer11858@gmail.com>
2023-11-30 14:54:54 +0600

    single child scroll view added

--- other_flutter_project/advance_basics/lib/queations_summary.dart
+++ other_flutter_project/advance_basics/lib/queations_summary.dart
@@ -9,26 +9,31 @@ class QuestionsSummary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: summaryData.map(
-        (data) {
-          return Row(
-            children: [
-              Text("${(int.parse(data['question_index'].toString()) +1)}"),
-             // Text(((data['question_index'] as Int) +1).toString()),
-              Expanded(
-                child: Column(
-                  children: [
-                 Text("${data['question']}"),
-                    SizedBox(height: 5,),
-                    Text("${data['user_answer']}"),
-                    Text("${data['correct_answer']}"),
-                  ],
-                ),
-              )
-            ],);
-        },
-      ).toList(),
+    return SizedBox(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: summaryData.map(
+            (data) {
+              return Row(
+                children: [
+                  Text("${(int.parse(data['question_index'].toString()) +1)}"),
+                 // Text(((data['question_index'] as Int) +1).toString()),
+                  Expanded(
+                    child: Column(
+                      children: [
+                     Text("${data['question']}"),
+                        SizedBox(height: 5,),
+                        Text("${data['user_answer']}"),
+                        Text("${data['correct_answer']}"),
+                      ],
+                    ),
+                  )
+                ],);
+            },
+          ).toList(),
+        ),
+      ),
     );
   }
 }
