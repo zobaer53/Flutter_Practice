74c405c32d4faf221a5b9346366885f684126abf
zobaer53 <hossain.zobaer11858@gmail.com>
2023-11-30 17:07:19 +0600

    expenseTracker-- model added

---
+++ other_flutter_project/expense_tracker/lib/expenses.dart
@@ -0,0 +1,28 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Expenses  extends StatefulWidget{
+  const Expenses({super.key});
+
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+
+}
+
+class _ExpensesState extends State<Expenses>{
+  @override
+  Widget build(BuildContext context) {
+  return const Scaffold(
+    body: Column(
+      children: [
+        Text("charts"),
+        Text("expense list")
+      ],
+    ),
+  );
+  }
+}
