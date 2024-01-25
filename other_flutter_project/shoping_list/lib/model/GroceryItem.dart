e4784963de0d6ce1a6593fab4a3dd2f9e2462bf9
zobaer53 <hossain.zobaer11858@gmail.com>
2024-01-25 18:58:12 +0600

    refactor -- shopping list git

---
+++ other_flutter_project/shoping_list/lib/model/GroceryItem.dart
@@ -0,0 +1,15 @@
+import 'Category.dart';
+
+class GroceryItem {
+  GroceryItem({
+    required this.id,
+    required this.name,
+    required this.quantity,
+    required this.category,
+  });
+
+  final String id;
+  final String name;
+  final int quantity;
+  final Category category;
+}
