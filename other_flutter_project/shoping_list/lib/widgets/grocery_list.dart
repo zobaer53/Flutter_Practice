f90625556dfc57ea3f2f846e63c3ef8ad4c822ac
zobaer53 <hossain.zobaer11858@gmail.com>
2024-01-25 19:28:49 +0600

    feature -- new Item screen created

--- other_flutter_project/shoping_list/lib/widgets/grocery_list.dart
+++ other_flutter_project/shoping_list/lib/widgets/grocery_list.dart
@@ -1,16 +1,33 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shoping_list/data/dummy_items.dart';
+import 'package:shoping_list/widgets/new_item.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+ void _addItem(){
+Navigator.push(context, MaterialPageRoute(builder: (ctx) => NewItem()));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Groceries'),
+        actions: [
+          IconButton(onPressed: (){
+            _addItem();
+          },
+              icon: Icon(Icons.add))
+        ],
       ),
+
       body: ListView.builder(
           itemCount: groceryItems.length,
           itemBuilder: (ctx, index) => ListTile(
@@ -21,7 +38,8 @@ class GroceryList extends StatelessWidget {
               color: groceryItems[index].category.color,
             ),
             trailing: Text("${groceryItems[index].quantity}"),
-              )),
+              )
+      ),
     );
   }
 }
